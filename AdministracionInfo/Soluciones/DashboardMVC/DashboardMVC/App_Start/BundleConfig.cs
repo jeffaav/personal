30423df6a28d4b6e15b79b9d742e5a74fc937ec0
Jeffrey <jeff.aav@gmail.com>
2015-11-24 17:45:26 -0500

    solucion

---
+++ AdministracionInfo/Soluciones/DashboardMVC/DashboardMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DashboardMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundle-css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundle-js").Include(
+                "~/Scritps/jquery-2.1.4.js",
+                "~/Scritps/modernizr-2.6.2.js",
+                "~/Scritps/bootstrap.js",
+                "~/Scritps/d3.js"
+            ));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
